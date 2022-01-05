2d8e2e4142c772ed9386582b1dcd604533edb260
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-05 14:32:57 -0500

    add component ProductInfo and UserInfo

---
+++ container-components/src/components/ProductInfo.js
@@ -0,0 +1,13 @@
+export const ProductInfo = ({ product }) => {
+  const { name, price, description, rating } = product;
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <p>{price}</p>
+      <h5>Description:</h5>
+      <p>{description}</p>
+      <p>Average Rating: {rating}</p>
+    </>
+  );
+};
