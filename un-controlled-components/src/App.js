8ec153bb43af9c308d1e72d8db183858f9e3830e
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-05 23:40:27 -0500

    handle controlled and uncontrolled modal

--- un-controlled-components/src/App.js
+++ un-controlled-components/src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
+import { useState } from 'react';
+
 import { ControlledForm } from './ControlledForm';
 import { UncontrolledForm } from './UncontrolledForm';
+import { UncontrolledModal } from './UncontrolledModal';
+import { ControlledModal } from './ControlledModal';
+
+import './App.css';
+
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="App">
       <h2>Controlled and Uncontrolled Components</h2>
-      <ControlledForm />
+      <ControlledModal showModal={showModal} onClose={() => setShowModal(false)}>
+        <h1>Hello!!</h1>
+      </ControlledModal>
+      <button onClick={() => setShowModal(true)}>
+        {showModal ? 'Hide Modal' : 'Show Modal'}
+      </button>
     </div>
   );
 }
