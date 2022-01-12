58387b97ea3384354c1b8300edf81791fdc293fa
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-12 13:45:52 -0500

    implment useDataSource custom hook

--- custom-hooks/src/components/UserInfo.js
+++ custom-hooks/src/components/UserInfo.js
@@ -1,9 +1,24 @@
+import axios from 'axios';
+
 import { useCurrentUser } from '../hooks/useCurrentUser';
 import { useUser } from '../hooks/useUser';
 import { useResource } from '../hooks/useResource';
+import { useDataResource } from '../hooks/useDataSource';
+
+const serverResource = url => async () => {
+  const response = await axios.get(url);
+  return response.data
+}
+
+const localStorageResource = key => () => {
+  return localStorage.getItem(key);
+}
 
 export const UserInfo = ({ userId }) => {
-  const user = useResource(`/users/${userId}`);
+  // const user = useResource(`/users/${userId}`);
+  const user = useDataResource(serverResource(`/users/${userId}`));
+  const  message = useCurrentUser(localStorageResource('message'));
+
   const { name, age, hairColor, hobbies } = user || {};
 
   return user ? (
