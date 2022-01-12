d6a0b756041cb03598b04b1c5abdab0b9ed79eef
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-12 15:15:02 -0500

    use recursive component

--- functional-programming/src/App.js
+++ functional-programming/src/App.js
@@ -1,7 +1,11 @@
+import { RecursiveComponent } from "./components/RecursiveComponent";
+import { nestedObject } from "./nestedObject";
+
 function App() {
   return (
     <div className="App">
       <h3>Functional Programming</h3>
+      <RecursiveComponent data={nestedObject} />
     </div>
   );
 }
---
+++ functional-programming/src/nestedObject.js
@@ -0,0 +1,21 @@
+export const nestedObject = {
+  a: 1,
+  b: {
+    b1: 4,
+    b2: {
+      b21: 'Hello',
+    },
+    b3: {
+      b31: {
+        message: 'Hi',
+      },
+      b32: {
+        message: 'Welcome',
+      }
+    }
+  },
+  c: {
+    c1: 2,
+    c2: 3,
+  }
+};
